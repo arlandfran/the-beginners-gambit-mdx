648cf64ed134899dfe5de10099c9a0bc4ad71a7a
Arland Torres <arland.torres@outlook.com>
2023-11-18 01:23:55 +0000

    Make sidebar nav sticky

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -9,26 +9,28 @@ export function SidebarNav() {
   const pathname = usePathname();
 
   return (
-    <aside className="hidden min-w-fit gap-4 p-8 sm:flex sm:flex-col">
-      {siteLinks.map((item, index) => (
-        <section key={index} className="flex flex-col gap-1">
-          <h4 className="text-sm font-semibold">{item.title}</h4>
-          {item.items.map((item, index) => (
-            <Link
-              key={index}
-              href={item.href}
-              className={cn(
-                "w-fit text-sm hover:underline",
-                pathname === item.href
-                  ? "font-medium text-foreground"
-                  : "text-muted-foreground",
-              )}
-            >
-              {item.title}
-            </Link>
-          ))}
-        </section>
-      ))}
-    </aside>
+    <div className="hidden min-w-fit sm:block">
+      <aside className="sticky top-24 flex min-w-fit flex-col gap-4 px-8">
+        {siteLinks.map((item, index) => (
+          <section key={index} className="flex flex-col gap-1">
+            <h4 className="text-sm font-semibold">{item.title}</h4>
+            {item.items.map((item, index) => (
+              <Link
+                key={index}
+                href={item.href}
+                className={cn(
+                  "w-fit text-sm hover:underline",
+                  pathname === item.href
+                    ? "font-medium text-foreground"
+                    : "text-muted-foreground",
+                )}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </section>
+        ))}
+      </aside>
+    </div>
   );
 }
